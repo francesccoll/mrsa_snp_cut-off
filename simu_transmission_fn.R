76cb0e800c86d73c8b2879a2f894c6108052a1ac
francesccoll <51442218+francesccoll@users.noreply.github.com>
2021-02-08 16:06:42 +0000

    Update simu_transmission_fn.R

--- simu_transmission_fn.R
+++ simu_transmission_fn.R
@@ -1,7 +1,7 @@
 ###### Functions for simulating transmission ####
 
 # If you find this code useful, please cite:
-# Coll et al. Peacock SJ. Definition of a genetic relatedness cutoff to exclude recent transmission of meticillin-resistant Staphylococcus aureus: a genomic epidemiology analysis. Lancet Microbe. 2020 Dec;1(8):e328-e335. doi: 10.1016/S2666-5247(20)30149-X. PMID: 33313577; PMCID: PMC7721685. (https://www.thelancet.com/journals/lanmic/article/PIIS2666-5247(20)30149-X/fulltext)
+# Coll et al. Definition of a genetic relatedness cutoff to exclude recent transmission of meticillin-resistant Staphylococcus aureus: a genomic epidemiology analysis. Lancet Microbe. 2020 Dec;1(8):e328-e335. doi: 10.1016/S2666-5247(20)30149-X. PMID: 33313577; PMCID: PMC7721685. (https://www.thelancet.com/journals/lanmic/article/PIIS2666-5247(20)30149-X/fulltext)
 
 ###*** Functions ***############################################################################################################
 
